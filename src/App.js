ade63d280418d312add161e489c695990a84cf7e
drd-2rcl <drd.rclrd@gmail.com>
2019-04-14 18:32:40 -0300

    import component main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Main from './pages/Main';
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">EAE
-      </div>
-    );
+    return <Main />
   }
 }
 
