dfce2e350e2de23d7272bc3607bbb6a1023d7c09
drd-2rcl <drd.rclrd@gmail.com>
2019-04-14 23:54:48 -0300

    a little logic inside box to call data api

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
+import { MdInsertDriveFile } from 'react-icons/md'
 import "./styles.css";
+import logo from '../../assets/Xfce_logo.svg'
+import api from '../../services/api'
+import { distanceInWords } from 'date-fns'
+import pt from 'date-fns/locale/pt';
+
 
 export default class Box extends Component {
+  state = { box: {} }
+
+  async componentDidMount() {
+    const box = this.props.match.params.id;
+    const response = await api.get(`boxes/${box}`);
+
+    this.setState({ box: response.data })
+  }
+
   render() {
     return (
-      <div>
-        
+      <div id="box-container">
+        <header>
+          <img src={logo} alt="" />
+          <h1>{this.state.box.title}</h1>
+        </header>
+
+        <ul>
+          { this.state.box.files ? this.state.box.files.map(file => (
+            <li>
+              <a className="fileInfo" href={file.url} target="_blank" >
+                <MdInsertDriveFile size={24} color="#A5CFFF" />
+                <strong>{file.title}</strong>
+              </a>
+              <span>há{" "}{distanceInWords(file.createdAt, new Date(), {
+                locale: pt
+              })}</span>
+            </li>
+            )) 
+          : null
+          }
+        </ul>
       </div>
     )
   }
